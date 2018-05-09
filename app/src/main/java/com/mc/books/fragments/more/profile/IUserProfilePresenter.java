e395ffe6c3157aa5053c656282896ae74ee2bfaa
minhvlkt.k56@gmail.com <matkhau1>
2018-05-09 11:11:37 +0700

    add select Image Avatar

--- app/src/main/java/com/mc/books/fragments/more/profile/IUserProfilePresenter.java
+++ app/src/main/java/com/mc/books/fragments/more/profile/IUserProfilePresenter.java
@@ -1,9 +1,12 @@
 package com.mc.books.fragments.more.profile;
 
+import android.graphics.Bitmap;
+
 import com.hannesdorfmann.mosby3.mvp.MvpPresenter;
 import com.hannesdorfmann.mosby3.mvp.MvpView;
 
 public interface IUserProfilePresenter<V extends MvpView> extends MvpPresenter<V> {
         void checkEmail(String string);
         void checkPhoneNumber(String string);
+
 }
